7e6a2921e94667ec75048233faed8149d60310c8
juliomolinadev <jmolina.dex@gmail.com>
2020-09-25 22:03:09 -0700

    Trabajando en modal para usuario

---
+++ src/components/modals/PadronModal.js
@@ -0,0 +1,162 @@
+import React from "react";
+import Datatable from "react-data-table-component";
+import styled from "styled-components";
+import { padron } from "../../data/padron";
+import differenceBy from "lodash/differenceBy";
+
+const title = "Seleccionar usuario";
+const data = padron;
+const columns = [
+	{ name: "CUENTA", selector: "CUENTA", sortable: true, width: "80px" },
+	{ name: "SUBCTA", selector: "SUBCTA", sortable: true, width: "60px", center: true },
+	{ name: "UNIDAD", selector: "UNIDAD", sortable: true, width: "60px", center: true },
+	{ name: "ZONA", selector: "ZONA", sortable: true, width: "60px", center: true },
+	{ name: "SECCION", selector: "SECCION", sortable: true, width: "60px", center: true },
+	{ name: "CP", selector: "CP", sortable: true, width: "60px", center: true },
+	{ name: "LT", selector: "LT", sortable: true, width: "60px", center: true },
+	{ name: "SLT", selector: "SLT", sortable: true, width: "60px", center: true },
+	{ name: "RA", selector: "RA", sortable: true, width: "60px", center: true },
+	{ name: "SRA", selector: "SRA", sortable: true, width: "60px", center: true },
+	{ name: "SSRA", selector: "SSRA", sortable: true, width: "60px", center: true },
+	{ name: "PCONTROL", selector: "PCONTROL", sortable: true, width: "80px", center: true },
+	{ name: "TENENCIA", selector: "TENENCIA", sortable: true, width: "60px", center: true },
+	{ name: "ESTADO", selector: "ESTADO", sortable: true, width: "60px", center: true },
+	{ name: "MUNICIPIO", selector: "MUNICIPIO", sortable: true, width: "60px", center: true },
+	{ name: "EJIDO", selector: "EJIDO", sortable: true, width: "60px", center: true },
+	{ name: "GRUPO", selector: "GRUPO", sortable: true, width: "60px", center: true },
+	{ name: "PREDIO", selector: "PREDIO", sortable: true, width: "60px", center: true },
+	{ name: "SISTRIEGO", selector: "SISTRIEGO", sortable: true, width: "60px", center: true },
+	{ name: "EQUIPO", selector: "EQUIPO", sortable: true, width: "60px", center: true },
+	{ name: "APPATERNO", selector: "APPATERNO", sortable: true },
+	{ name: "APMATERNO", selector: "APMATERNO", sortable: true },
+	{ name: "NOMBRE", selector: "NOMBRE", sortable: true },
+	{ name: "SUPFISICA", selector: "SUPFISICA", sortable: true, width: "80px", center: true },
+	{ name: "SUPRIEGO", selector: "SUPRIEGO", sortable: true, width: "60px", center: true },
+	{ name: "FECHA", selector: "FECHA", sortable: true },
+	{ name: "REFERENCIA", selector: "REFERENCIA", sortable: true, width: "80px", center: true },
+	{ name: "MODULO", selector: "MODULO", sortable: true, width: "60px", center: true }
+];
+
+const TextField = styled.input`
+	height: 32px;
+	width: 200px;
+	border-radius: 3px;
+	border-top-left-radius: 5px;
+	border-bottom-left-radius: 5px;
+	border-top-right-radius: 0;
+	border-bottom-right-radius: 0;
+	border: 1px solid #e5e5e5;
+	padding: 0 32px 0 16px;
+
+	&:hover {
+		cursor: pointer;
+	}
+`;
+
+const FilterComponent = ({ filterText, onFilter, onClear }) => (
+	<>
+		<TextField
+			id="search"
+			type="text"
+			placeholder="Filtrar por apellido"
+			value={filterText}
+			onChange={onFilter}
+		/>
+		<button className=" btn btn-primary btn-sm" type="button" onClick={onClear}>
+			<i className="fas fa-redo"></i>
+		</button>
+	</>
+);
+
+export const PadronModal = () => {
+	const [filterText, setFilterText] = React.useState("");
+	const [resetPaginationToggle, setResetPaginationToggle] = React.useState(false);
+	let filteredItems = data.filter(
+		(item) =>
+			item["APPATERNO"] && item["APPATERNO"].toLowerCase().includes(filterText.toLowerCase())
+	);
+
+	const subHeaderComponentMemo = React.useMemo(() => {
+		const handleClear = () => {
+			if (filterText) {
+				setResetPaginationToggle(!resetPaginationToggle);
+				setFilterText("");
+			}
+		};
+
+		return (
+			<FilterComponent
+				onFilter={(e) => setFilterText(e.target.value)}
+				onClear={handleClear}
+				filterText={filterText}
+			/>
+		);
+	}, [filterText, resetPaginationToggle]);
+
+	const paginationOptions = {
+		rowsPerPageText: "Filas por pagina",
+		rangesSeparatorText: "de",
+		selectAllRowsItem: true,
+		selectAllRowsItemText: "Todos"
+	};
+
+	const [selectedRows, setSelectedRows] = React.useState([]);
+	const [disableRows, setDisableRows] = React.useState([]);
+	const [toggleCleared, setToggleCleared] = React.useState(false);
+
+	const handleRowSelected = React.useCallback((state) => {
+		setSelectedRows(state.selectedRows);
+	}, []);
+
+	const contextActions = React.useMemo(() => {
+		const handleSelect = () => {
+			window.alert(`Cuenta: ${selectedRows.map((r) => r.CUENTA)}`);
+			setToggleCleared(!toggleCleared);
+		};
+
+		return (
+			<button className=" btn btn-primary btn-sm" type="button" onClick={handleSelect}>
+				<span>Seleccionar </span>
+				<i className="fas fa-download"></i>
+			</button>
+		);
+	}, [filteredItems, selectedRows, toggleCleared]);
+
+	const rowDisabledCriteria = (row) => row.isOutOfStock;
+	// filteredItems = filteredItems.map((datum) => ({ ...datum, isOutOfStock: true }));
+
+	filteredItems[1].isOutOfStock = false;
+	filteredItems[3].isOutOfStock = true;
+
+	// setData(differenceBy(filteredItems, selectedRows, 'CUENTA'));
+
+	return (
+		<div className="table-responsive">
+			<Datatable
+				columns={columns}
+				data={filteredItems}
+				title={title}
+				pagination
+				paginationComponentOptions={paginationOptions}
+				paginationResetDefaultPage={resetPaginationToggle}
+				subHeaderComponent={subHeaderComponentMemo}
+				fixedHeader
+				subHeader
+				persistTableHead
+				fixedHeaderScrollHeight="400px"
+				highlightOnHover={true}
+				striped={true}
+				pointerOnHover={true}
+				dense={true}
+				selectableRows
+				selectableRowsNoSelectAll
+				selectableRowsHighlight
+				// actions={actions}
+				contextActions={contextActions}
+				onSelectedRowsChange={handleRowSelected}
+				clearSelectedRows={toggleCleared}
+				selectableRowDisabled={rowDisabledCriteria}
+			/>
+		</div>
+	);
+};
---
+++ src/components/permisos/NuevoPermisoScreen.js
@@ -0,0 +1,217 @@
+import React from "react";
+import { PermisoModal } from "./PermisoModal";
+
+export const NuevoPermisoScreen = () => {
+	return (
+		<>
+			<div className="row m-3 d-flex justify-content-center">
+				<h1>Alta de permiso unico de siembra</h1>
+			</div>
+			<form className="container pb-4">
+				<div className="row">
+					<div className="col-sm-6">
+						<div className="form-group">
+							<label>Usuario:</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Usuario"
+								name="usuario"
+								autoComplete="off"
+							/>
+						</div>
+					</div>
+					<div className="col-sm-6">
+						<div className="form-group">
+							<label>Productor:</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Productor"
+								name="productor"
+								autoComplete="off"
+							/>
+						</div>
+					</div>
+				</div>
+
+				<div className="row">
+					<div className="col-sm-6">
+						<div className="form-group">
+							<label>Cultivo:</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="cultivo"
+								name="cultivo"
+								autoComplete="off"
+							/>
+						</div>
+					</div>
+					<div className="col-sm-6">
+						<div className="form-group">
+							<label>Variedad:</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="variedad"
+								name="variedad"
+								autoComplete="off"
+							/>
+						</div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-sm-6">
+						<div className="form-group">
+							<label>Superficie:</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="superficie"
+								name="superficie"
+								autoComplete="off"
+							/>
+						</div>
+					</div>
+					<div className="col-sm-6">
+						<div className="form-group">
+							<label>Fuente de credito:</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="fuente-credito"
+								name="fuente-credito"
+								autoComplete="off"
+							/>
+						</div>
+					</div>
+				</div>
+
+				<div className="row">
+					<div className="col-sm-6">
+						<div className="form-group">
+							<label>Latitud:</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="latitud"
+								name="latitud"
+								autoComplete="off"
+							/>
+						</div>
+					</div>
+					<div className="col-sm-6">
+						<div className="form-group">
+							<label>Longitud:</label>
+							<input
+								type="text"
+								className="form-control"
+								placeholder="longitud"
+								name="longitud"
+								autoComplete="off"
+							/>
+						</div>
+					</div>
+				</div>
+
+				<div className="row">
+					<div className="col-sm-6">
+						<div className="form-group">
+							<label>Tipo:</label>
+							<br />
+							<div className="row">
+								<div className="px-4">
+									<input type="radio" id="normal" name="tipo" value="normal" />
+									<span> </span>
+									<label htmlFor="normal"> Normal</label>
+								</div>
+								<div className="px-4">
+									<input type="radio" id="superficie-extra" name="tipo" value="superficie-extra" />
+									<span> </span>
+									<label htmlFor="superficie-extra"> Superficie extra</label>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-sm-6">
+						<div className="form-group">
+							<label>Sistema:</label>
+							<br />
+							<div className="row">
+								<div className="px-4">
+									<input type="checkbox" id="gravedad" name="sistema" value="gravedad" />
+									<span> </span>
+									<label htmlFor="gravedad">Gravedad</label>
+								</div>
+								<div className="px-4">
+									<input
+										type="checkbox"
+										id="poso-particular"
+										name="sistema"
+										value="poso-particular"
+									/>
+									<span> </span>
+									<label htmlFor="poso-particular"> Poso particular</label>
+								</div>
+								<div className="px-4">
+									<input type="checkbox" id="poso-federal" name="sistema" value="poso-federal" />
+									<span> </span>
+									<label htmlFor="poso-federal"> Poso federal</label>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div className="form-group">
+					<textarea
+						type="text"
+						className="form-control"
+						placeholder="Observaciones"
+						rows="5"
+						name="notas"
+					></textarea>
+					<small id="emailHelp" className="form-text text-muted">
+						Información adicional
+					</small>
+				</div>
+
+				<button type="submit" className="btn btn-outline-primary btn-block">
+					<i className="far fa-save"></i>
+					<span> Guardar</span>
+				</button>
+			</form>
+			<PermisoModal />
+		</>
+	);
+};
+
+// Ctrl + Shift + W R   : Emmet wrap.
+// Alt + ↑ / ↓          : Mueve el contenido de una linea o seleccion.
+// Shift + Tab          : Quita la indentacion de un bloque seleccionado.
+// Alt + Shift + ↑ / ↓  : Clona la linea actual o bloque seleccionado abajo o arriba.
+// Ctrl + Shift + L     : Selecciona todas las ocurrencias de la seleccion.
+// Ctrl + Shift + K     : Borra la linea actual.
+// Ctrl + Shift + ↑ / ↓ : Crea multicursores continuos.
+// Ctrl + Alt + U       : Combierte seleccion en mayusculas.
+// Ctrl + D             : Selecciona la siguiente ocurencia con multicursor.
+
+// Ctrl + P             : [Input] Lista de busqueda para los archivos del proyecto
+// Ctrl + Shift + P     : [Input + >] Lista de busqueda para comandos.
+// Ctrl + Shift + O     : [Input + @] Lista de busqueda para las definiciones de elementos (: ordena).
+// Ctrl + G             : [Input + :] Buscar una linea en especifico.
+
+// Ctrl + Alt + clk der : Abre tab con la definicion del elemento en cursor, lo crea si no existe.
+// Ctrl + Shift + F12   : Abre una ventana que muestra la definicion del elemento en cursor.
+// F2                   : Refactorizar, rename.
+
+// Ctrl + K Z           : Modo zen.
+// Ctrl + `             : Abre la terminal.
+// Ctrl + K Ctrl + S    : Configuracion de shortcuts.
+// Ctrl + Shift + V     : Abre archivo en markdown para previsualizar.
+// Shift + Alt + S      : Guarda todos los archivos.
+// Ctrl + space         : Recupera el autocompletado.
+
+// Ctrl + /             : Comenta la linea donde se encuentre el cursor o el bloque seleccionado.
+// Ctrl + Shift + A     : Comenta solo el fragmento seleccionado.
