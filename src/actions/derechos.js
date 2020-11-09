ffbf4453c0eae1e4da6f4dc10ac7e86e37d802e9
juliomolinadev <jmolina.dex@gmail.com>
2020-11-08 22:21:17 -0800

    Define las variables para crear nuevo permiso

--- src/actions/derechos.js
+++ src/actions/derechos.js
@@ -1,120 +1,120 @@
-import Swal from "sweetalert2";
+// import Swal from "sweetalert2";
 
-import { db } from "../firebase/firebase-config";
-import { types } from "../types/types";
-import { loadDerechos } from "../helpers/loadDerechos";
-import { fileUpload } from "../helpers/fileUpload";
+// import { db } from "../firebase/firebase-config";
+// import { types } from "../types/types";
+// import { loadDerechos } from "../helpers/loadDerechos";
+// import { fileUpload } from "../helpers/fileUpload";
 
-export const startNewDerecho = () => {
-	return async (dispatch, getState) => {
-		const { uid } = getState().auth;
+// export const startNewDerecho = () => {
+// 	return async (dispatch, getState) => {
+// 		const { uid } = getState().auth;
 
-		const newDerecho = {
-			title: "",
-			body: "",
-			date: new Date().getTime()
-		};
+// 		const newDerecho = {
+// 			title: "",
+// 			body: "",
+// 			date: new Date().getTime()
+// 		};
 
-		const doc = await db.collection(`derechos`).add(newDerecho);
+// 		const doc = await db.collection(`derechos`).add(newDerecho);
 
-		dispatch(activeDerecho(doc.id, newDerecho));
-		dispatch(addNewDerecho(doc.id, newDerecho));
-	};
-};
+// 		dispatch(activeDerecho(doc.id, newDerecho));
+// 		dispatch(addNewDerecho(doc.id, newDerecho));
+// 	};
+// };
 
-export const activeDerecho = (id, derecho) => ({
-	type: types.derechosActive,
-	payload: {
-		id,
-		...derecho
-	}
-});
+// export const activeDerecho = (id, derecho) => ({
+// 	type: types.derechosActive,
+// 	payload: {
+// 		id,
+// 		...derecho
+// 	}
+// });
 
-export const addNewDerecho = (id, derecho) => ({
-	type: types.derechosAddNew,
-	payload: {
-		id,
-		...derecho
-	}
-});
+// export const addNewDerecho = (id, derecho) => ({
+// 	type: types.derechosAddNew,
+// 	payload: {
+// 		id,
+// 		...derecho
+// 	}
+// });
 
-export const startLoadingDerechos = (apPaterno, claveEntidad) => {
-	return async (dispatch) => {
-		const derechos = await loadDerechos(apPaterno, claveEntidad);
-		dispatch(setDerechos(derechos));
-	};
-};
+// export const startLoadingDerechos = (apPaterno, claveEntidad) => {
+// 	return async (dispatch) => {
+// 		const derechos = await loadDerechos(apPaterno, claveEntidad);
+// 		dispatch(setDerechos(derechos));
+// 	};
+// };
 
-export const setDerechos = (derechos) => ({
-	type: types.derechosLoad,
-	payload: derechos
-});
+// export const setDerechos = (derechos) => ({
+// 	type: types.derechosLoad,
+// 	payload: derechos
+// });
 
-export const startSaveDerecho = (derecho) => {
-	return async (dispatch, getState) => {
-		const { uid } = getState().auth;
+// export const startSaveDerecho = (derecho) => {
+// 	return async (dispatch, getState) => {
+// 		const { uid } = getState().auth;
 
-		if (!derecho.url) {
-			delete derecho.url;
-		}
+// 		if (!derecho.url) {
+// 			delete derecho.url;
+// 		}
 
-		const derechoToFirestore = { ...derecho };
-		delete derechoToFirestore.id;
+// 		const derechoToFirestore = { ...derecho };
+// 		delete derechoToFirestore.id;
 
-		await db.doc(`${uid}/journal/derechos/${derecho.id}`).update(derechoToFirestore);
+// 		await db.doc(`${uid}/journal/derechos/${derecho.id}`).update(derechoToFirestore);
 
-		dispatch(refreshDerecho(derecho.id, derechoToFirestore));
-		Swal.fire("Saved", derecho.title, "success");
-	};
-};
+// 		dispatch(refreshDerecho(derecho.id, derechoToFirestore));
+// 		Swal.fire("Saved", derecho.title, "success");
+// 	};
+// };
 
-export const refreshDerecho = (id, derecho) => ({
-	type: types.derechosUpdated,
-	payload: {
-		id,
-		derecho: {
-			id,
-			...derecho
-		}
-	}
-});
+// export const refreshDerecho = (id, derecho) => ({
+// 	type: types.derechosUpdated,
+// 	payload: {
+// 		id,
+// 		derecho: {
+// 			id,
+// 			...derecho
+// 		}
+// 	}
+// });
 
-export const startUploading = (file) => {
-	return async (dispatch, getState) => {
-		const { active: activeDerecho } = getState().derechos;
+// export const startUploading = (file) => {
+// 	return async (dispatch, getState) => {
+// 		const { active: activeDerecho } = getState().derechos;
 
-		Swal.fire({
-			title: "Uploading...",
-			text: "Please wait...",
-			allowOutsideClick: false,
-			onBeforeOpen: () => {
-				Swal.showLoading();
-			}
-		});
+// 		Swal.fire({
+// 			title: "Uploading...",
+// 			text: "Please wait...",
+// 			allowOutsideClick: false,
+// 			onBeforeOpen: () => {
+// 				Swal.showLoading();
+// 			}
+// 		});
 
-		const fileUrl = await fileUpload(file);
-		activeDerecho.url = fileUrl;
+// 		const fileUrl = await fileUpload(file);
+// 		activeDerecho.url = fileUrl;
 
-		dispatch(startSaveDerecho(activeDerecho));
+// 		dispatch(startSaveDerecho(activeDerecho));
 
-		Swal.close();
-	};
-};
+// 		Swal.close();
+// 	};
+// };
 
-export const startDeleting = (id) => {
-	return async (dispatch, getState) => {
-		const uid = getState().auth.uid;
-		await db.doc(`${uid}/journal/derechos/${id}`).delete();
+// export const startDeleting = (id) => {
+// 	return async (dispatch, getState) => {
+// 		const uid = getState().auth.uid;
+// 		await db.doc(`${uid}/journal/derechos/${id}`).delete();
 
-		dispatch(deleteDerecho(id));
-	};
-};
+// 		dispatch(deleteDerecho(id));
+// 	};
+// };
 
-export const deleteDerecho = (id) => ({
-	type: types.derechosDelete,
-	payload: id
-});
+// export const deleteDerecho = (id) => ({
+// 	type: types.derechosDelete,
+// 	payload: id
+// });
 
-export const derechoLogout = () => ({
-	type: types.derechosLogoutCleaning
-});
+// export const derechoLogout = () => ({
+// 	type: types.derechosLogoutCleaning
+// });
--- src/types/types.js
+++ src/types/types.js
@@ -10,12 +10,12 @@ export const types = {
 	uiStartLoading: "[UI] Start Loading",
 	uiFinishLoading: "[UI] Finish Loading",
 
-	derechosAddNew: "[Derechos] New derecho",
-	derechosActive: "[Derechos] Set active derecho",
-	derechosLoad: "[Derechos] Load derechos",
-	derechosUpdated: "[Derechos] Updated derecho",
-	derechosDelete: "[Derechos] Delete derecho",
-	derechosLogoutCleaning: "[Derechos] Logout Cleaning",
+	// derechosAddNew: "[Derechos] New derecho",
+	// derechosActive: "[Derechos] Set active derecho",
+	// derechosLoad: "[Derechos] Load derechos",
+	// derechosUpdated: "[Derechos] Updated derecho",
+	// derechosDelete: "[Derechos] Delete derecho",
+	// derechosLogoutCleaning: "[Derechos] Logout Cleaning",
 
 	altaPermisoOpenCultivosModal: "[Alta Permisos] Open cultivos modal",
 	altaPermisoCloseCultivosModal: "[Alta Permisos] Close cultivos modal",
