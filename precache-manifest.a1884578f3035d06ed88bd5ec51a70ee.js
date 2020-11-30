1a65d82005606b4aeab912700ad5a83b7d3d1c8e
juliomolinadev <jmolina.dex@gmail.com>
2020-11-29 22:21:48 -0800

    Updates

---
+++ precache-manifest.a1884578f3035d06ed88bd5ec51a70ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5090d25543f2f86f968a2ffa83e579",
+    "url": "/sicper/index.html"
+  },
+  {
+    "revision": "07e86e1936dca1ce8a3a",
+    "url": "/sicper/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e3afaac6795adbbc1c23",
+    "url": "/sicper/static/css/main.13984551.chunk.css"
+  },
+  {
+    "revision": "07e86e1936dca1ce8a3a",
+    "url": "/sicper/static/js/2.a641b4a0.chunk.js"
+  },
+  {
+    "revision": "c0bfc3a02e690d18965b783c146bc540",
+    "url": "/sicper/static/js/2.a641b4a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3afaac6795adbbc1c23",
+    "url": "/sicper/static/js/main.97044a62.chunk.js"
+  },
+  {
+    "revision": "73201ec75c91d4a59e24",
+    "url": "/sicper/static/js/runtime-main.31a0d830.js"
+  }
+]);
