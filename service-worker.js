d5940eb7b1457ef9483d2014e699fa97bad81cb8
juliomolinadev <jmolina.dex@gmail.com>
2020-12-03 17:51:22 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74ea2016cd4c025a282a5363831ee69a.js"
+  "/precache-manifest.58d8c3ba286e0c337d9675c835e7e68b.js"
 );
 
 self.addEventListener('message', (event) => {
