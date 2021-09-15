eecc88e459f28bce002d01afb3bbf01a8a5b853b
juliomolinadev <jmolina.dex@gmail.com>
2021-09-14 19:58:31 -0700

    Limpia testArea

--- src/components/templates/TestArea.js
+++ src/components/templates/TestArea.js
@@ -1,28 +1,10 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { updatePadron } from "../../helpers/DB/updatePadron";
-import { FileInput } from "../ui/atoms/FileInput";
 
 export const TestArea = () => {
-	const { updatingPadron } = useSelector((state) => state.scenes.padronScreen);
-	const dispatch = useDispatch();
-
-	const startUpdatePadron = (file) => {
-		dispatch(updatePadron(file));
-	};
-
 	return (
 		<>
 			<div className="row justify-content-center pt-5">
 				<h1>TestArea</h1>
-			</div>
-			<div className="row justify-content-center pt-5">
-				<FileInput
-					actionFunction={startUpdatePadron}
-					title="Cargar Padron"
-					updating={updatingPadron}
-					fileExtension=".xlsx"
-				/>
 			</div>
 		</>
 	);
