acaf6ff8ef6bdcd948a1637956af82b393415932
juliomolinadev <jmolina.dex@gmail.com>
2021-09-15 21:14:00 -0700

    Updates

---
+++ precache-manifest.7b97e4f5c65134d0a8cbcffae3ed2be6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e067bf1d035cfd8074c03405ab2877bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07ed2fd2777c2d9035bc",
+    "url": "/static/css/2.f8236e86.chunk.css"
+  },
+  {
+    "revision": "18fdd79f2450a41d9eda",
+    "url": "/static/css/main.ae5b459d.chunk.css"
+  },
+  {
+    "revision": "07ed2fd2777c2d9035bc",
+    "url": "/static/js/2.d87ce4d9.chunk.js"
+  },
+  {
+    "revision": "10a280b98dbf2de163a8b88e212b76e2",
+    "url": "/static/js/2.d87ce4d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18fdd79f2450a41d9eda",
+    "url": "/static/js/main.c840fb11.chunk.js"
+  },
+  {
+    "revision": "625fe777cd15a7f7e360",
+    "url": "/static/js/runtime-main.fcf44fbc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90873001eeb16093400e91ba7e9b314e.js"
+  "/precache-manifest.7b97e4f5c65134d0a8cbcffae3ed2be6.js"
 );
 
 self.addEventListener('message', (event) => {
