899035da077d74b25227c4598fc30215ff41a5d2
juliomolinadev <jmolina.dex@gmail.com>
2021-09-25 17:29:20 -0700

    Inclulle sistema de riego en ficha de usuario seleccionado en el padron

--- src/components/padron/UsuarioSelectedDetail.js
+++ src/components/padron/UsuarioSelectedDetail.js
@@ -2,20 +2,21 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export const UsuarioSelectedDetail = () => {
-	const { usuarios, idUsuarioSelected, supPrevia } = useSelector((state) => state.altaPermisos);
-
-	const usuario = usuarios.find((usuario) => usuario.id === idUsuarioSelected);
+	const altaPermisos = useSelector((state) => state.altaPermisos);
 
 	return (
 		<div className="border rounded mb-4 p-2">
-			<div>Cuenta: {`${usuario.cuenta}.${usuario.subcta}`}</div>
-			<div>Usuario: {`${usuario.apPaterno} ${usuario.apMaterno} ${usuario.nombre}`}</div>
-			<div>Colonia/Ejido: {usuario.localidad}</div>
-			<div>Lote: {usuario.predio}</div>
-			<div>Seccion: {usuario.seccion}</div>
-			<div>Sup. Derecho: {usuario.supRiego}</div>
-			<div>Sup. Disponible: {usuario.supRiego - supPrevia}</div>
-			{usuario.reacomodo && <div className="text-info">Reacomodo: {usuario.reacomodo}</div>}
+			<div>Cuenta: {altaPermisos.cuenta}</div>
+			<div>Usuario: {altaPermisos.usuario}</div>
+			<div>Colonia/Ejido: {altaPermisos.localidad}</div>
+			<div>Lote: {altaPermisos.lote}</div>
+			<div>Seccion: {altaPermisos.seccion}</div>
+			<div>Sup. Derecho: {altaPermisos.supDerecho}</div>
+			<div>Sup. Disponible: {altaPermisos.supDerecho - altaPermisos.supPrevia}</div>
+			<div>Sistema: {altaPermisos.sistema}</div>
+			{altaPermisos.reacomodo && (
+				<div className="text-info">Reacomodo: {altaPermisos.reacomodo}</div>
+			)}
 		</div>
 	);
 };
