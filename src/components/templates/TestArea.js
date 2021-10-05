35d51c842922d330dad41a8e7c45b2f2c341024b
juliomolinadev <jmolina.dex@gmail.com>
2021-10-05 15:53:57 -0700

    Marca para verificar deploy

--- src/components/templates/TestArea.js
+++ src/components/templates/TestArea.js
@@ -5,7 +5,7 @@ export const TestArea = () => {
 	return (
 		<>
 			<div className="row justify-content-center pt-5">
-				<h1>TestArea</h1>
+				<h1>TestArea x</h1>
 			</div>
 
 			<div className="row">
