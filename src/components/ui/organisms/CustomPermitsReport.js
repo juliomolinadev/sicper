113a2586a69d179aa1c4141d1e6939eabd4d6822
julio <jmolina.dex@gmail.com>
2021-10-18 19:54:38 -0700

    Agrega filtros a reporte de permisos configurable

--- src/components/ui/organisms/CustomPermitsReport.js
+++ src/components/ui/organisms/CustomPermitsReport.js
@@ -5,7 +5,6 @@ import { useForm } from "../../../hooks/useForm";
 import { ReportModule } from "./ReportModule";
 
 export const CustomPermitsReport = () => {
-	const title = "Reporte de permisos";
 	const headers = [
 		{ id: "cuenta", header: "CUENTA", styles: "", sum: false, count: false },
 		{ id: "numeroPermiso", header: "PERMISO", styles: "", sum: false, count: true },
@@ -21,9 +20,23 @@ export const CustomPermitsReport = () => {
 	const { modulo, variablesGlobales } = useSelector((state) => state.auth);
 	const { cicloActual } = variablesGlobales;
 
-	const [formValues, handleInputChange] = useForm({ palabra: "" });
-	const { palabra } = formValues;
+	const [formValues, handleInputChange] = useForm({
+		palabra: "",
+		campo: "",
+		filter: "",
+		order1: ""
+	});
+	const { palabra, campo, filter, order1 } = formValues;
 	const [permisos, setPermisos] = useState([]);
+	console.log(permisos);
+
+	const getTitle = () => {
+		const campoForTitle = headers.find((header) => header.id === campo);
+		if (campoForTitle) return `REPORTE DE PERMISOS POR ${campoForTitle.header}`;
+		else return `REPORTE DE PERMISOS`;
+	};
+
+	const title = getTitle();
 
 	const onlyUnique = (objectsArray, key) => {
 		const unique = [];
@@ -43,33 +56,34 @@ export const CustomPermitsReport = () => {
 		return unique;
 	};
 
-	const filter = "usuario";
-	const order1 = "cultivo";
-
 	const getPermisos = async () => {
-		const permisosToSet = await simpleLoadPermits(palabra, "cuenta", modulo, cicloActual);
-		setPermisos(permisosToSet);
-		// setFilter();
+		if (campo.length) {
+			const permisosToSet = await simpleLoadPermits(palabra, campo, modulo, cicloActual);
+			setPermisos(permisosToSet);
+		}
 	};
 
 	const setFilter = () => {
-		const unique = onlyUnique(permisos, filter);
+		const cleanPermisos = permisos.filter(
+			(permiso) => permiso.cuenta !== "SUBTOTAL" && permiso.cuenta !== "TOTAL"
+		);
+		const unique = onlyUnique(cleanPermisos, filter);
 		const separateData = [];
 		const finalData = [];
 		const totalRow = { cuenta: "TOTAL" };
 
 		unique.forEach((value) => {
-			separateData.push(permisos.filter((permiso) => permiso[filter] === value));
+			separateData.push(cleanPermisos.filter((permiso) => permiso[filter] === value));
 		});
 
 		separateData.forEach((filterItem) => {
 			const subTotalRow = { cuenta: "SUBTOTAL" };
 
 			filterItem.sort((a, b) => {
-				if (a[order1] > b[order1]) {
+				if (a[order1] < b[order1]) {
 					return 1;
 				}
-				if (a[order1] < b[order1]) {
+				if (a[order1] > b[order1]) {
 					return -1;
 				}
 				return 0;
@@ -110,33 +124,103 @@ export const CustomPermitsReport = () => {
 
 	return (
 		<>
-			<div className="container d-flex mb-4 col-6">
-				<input
-					type="text"
-					className="form-control"
-					placeholder="Seccion"
-					name="palabra"
-					autoComplete="off"
-					value={palabra}
-					onChange={handleInputChange}
-					onKeyUp={handleKeyUp}
-				/>
+			<div className="container mb-4 col-sm-8">
+				<div className="row">
+					<div className="d-flex col-sm-8">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="PARAMETRO DE BUSQUEDA"
+							name="palabra"
+							autoComplete="off"
+							value={palabra}
+							onChange={handleInputChange}
+							onKeyUp={handleKeyUp}
+						/>
+					</div>
 
-				<button
-					className=" btn btn-outline-primary d-sm-block ml-auto"
-					type="button"
-					onClick={getPermisos}
-				>
-					<i className="fas fa-search"></i>
-				</button>
+					<div className="d-flex col-sm-4">
+						<select
+							name="campo"
+							type="text"
+							value={campo}
+							onChange={handleInputChange}
+							className="form-control ml-2"
+						>
+							<option hidden defaultValue={false}>
+								BUSCAR EN
+							</option>
 
-				<button
-					className=" btn btn-outline-primary d-sm-block ml-auto"
-					type="button"
-					onClick={() => setFilter()}
-				>
-					Filtrar
-				</button>
+							{headers.map((header) => (
+								<option key={header.id} value={header.id}>
+									{header.header}
+								</option>
+							))}
+						</select>
+
+						<button className="btn btn-outline-primary ml-2" type="button" onClick={getPermisos}>
+							<i className="fas fa-search"></i>
+						</button>
+					</div>
+				</div>
+
+				{permisos.length > 0 && (
+					<>
+						<div className="row mt-2">
+							<div className="d-flex col-sm-8">
+								<select
+									name="filter"
+									type="text"
+									value={filter}
+									onChange={handleInputChange}
+									className="form-control ml-2"
+								>
+									<option hidden defaultValue={false}>
+										ORDENAR POR
+									</option>
+
+									{headers.map((header) => (
+										<option key={header.id} value={header.id}>
+											{header.header}
+										</option>
+									))}
+								</select>
+							</div>
+						</div>
+
+						<div className="row mt-2">
+							<div className="d-flex col-sm-8">
+								<select
+									name="order1"
+									type="text"
+									value={order1}
+									onChange={handleInputChange}
+									className="form-control ml-2"
+								>
+									<option hidden defaultValue={false}>
+										ORDENAR POR
+									</option>
+
+									{headers.map((header) => (
+										<option key={header.id} value={header.id}>
+											{header.header}
+										</option>
+									))}
+								</select>
+							</div>
+
+							<div className="d-flex col-sm-4">
+								<button
+									className=" btn btn-outline-primary d-sm-block ml-2 "
+									type="button"
+									onClick={() => setFilter()}
+								>
+									Filtrar
+								</button>
+							</div>
+						</div>
+					</>
+				)}
 			</div>
 
 			{permisos.length > 0 && (
