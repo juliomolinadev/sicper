634ddd8ae884498749edbcc5265d0a082dc03aa4
julio <jmolina.dex@gmail.com>
2021-10-26 10:38:03 -0700

    Arregla detalle de cultivo seleccionado al expedir permisos

--- src/components/permisos/inputsNuevosPermisos/CultivoSelected.js
+++ src/components/permisos/inputsNuevosPermisos/CultivoSelected.js
@@ -2,10 +2,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export const CultivoSelected = () => {
-	const { nombreCultivo, claveCultivo, subciclo, superficiePreviaCultivo } = useSelector(
+	const { nombreCultivo, claveCultivo, subciclo, superficiePreviaCultivo, sistema } = useSelector(
 		(state) => state.altaPermisos
 	);
 	const { autorizadosPorCultivo } = useSelector((state) => state.autorizadosScreen);
+	console.log("sistema en lugar: ", sistema);
 
 	return (
 		<div className="border rounded mb-4 p-2">
@@ -40,81 +41,98 @@ export const CultivoSelected = () => {
 							</thead>
 
 							<tbody>
-								<tr>
-									<th scope="row">Gravedad Normal</th>
-									<td className="text-center">{autorizadosPorCultivo.gravedadNormalAsignada}</td>
-									<td className="text-center">{autorizadosPorCultivo.gravedadNormalAutorizada}</td>
-									<td className="text-center">{superficiePreviaCultivo.gravedadNormal}</td>
-									{autorizadosPorCultivo.gravedadNormalAutorizada -
-										superficiePreviaCultivo.gravedadNormal <=
-									0 ? (
-										<td className="text-center text-danger">
-											{autorizadosPorCultivo.gravedadNormalAutorizada -
-												superficiePreviaCultivo.gravedadNormal}
-										</td>
-									) : (
-										<td className="text-center">
-											{autorizadosPorCultivo.gravedadNormalAutorizada -
-												superficiePreviaCultivo.gravedadNormal}
-										</td>
-									)}
-								</tr>
-								<tr>
-									<th scope="row">Pozo Normal</th>
-									<td className="text-center">{autorizadosPorCultivo.pozoNormalAsignada}</td>
-									<td className="text-center">{autorizadosPorCultivo.pozoNormalAutorizada}</td>
-									<td className="text-center">{superficiePreviaCultivo.pozoNormal}</td>
-									{autorizadosPorCultivo.pozoNormalAutorizada -
-										superficiePreviaCultivo.pozoNormal <=
-									0 ? (
-										<td className="text-center text-danger">
-											{autorizadosPorCultivo.pozoNormalAutorizada -
-												superficiePreviaCultivo.pozoNormal}
-										</td>
-									) : (
-										<td className="text-center">
-											{autorizadosPorCultivo.pozoNormalAutorizada -
-												superficiePreviaCultivo.pozoNormal}
-										</td>
-									)}
-								</tr>
-								<tr>
-									<th scope="row">Gravedad Extra</th>
-									<td className="text-center">{autorizadosPorCultivo.gravedadExtraAsignada}</td>
-									<td className="text-center">{autorizadosPorCultivo.gravedadExtraAutorizada}</td>
-									<td className="text-center">{superficiePreviaCultivo.gravedadExtra}</td>
-									{autorizadosPorCultivo.gravedadExtraAutorizada -
-										superficiePreviaCultivo.gravedadExtra <=
-									0 ? (
-										<td className="text-center text-danger">
-											{autorizadosPorCultivo.gravedadExtraAutorizada -
-												superficiePreviaCultivo.gravedadExtra}
-										</td>
-									) : (
-										<td className="text-center">
-											{autorizadosPorCultivo.gravedadExtraAutorizada -
-												superficiePreviaCultivo.gravedadExtra}
-										</td>
-									)}
-								</tr>
-								<tr>
-									<th scope="row">Pozo Extra</th>
-									<td className="text-center">{autorizadosPorCultivo.pozoExtraAsignada}</td>
-									<td className="text-center">{autorizadosPorCultivo.pozoExtraAutorizada}</td>
-									<td className="text-center">{superficiePreviaCultivo.pozoExtra}</td>
-									{autorizadosPorCultivo.pozoExtraAutorizada - superficiePreviaCultivo.pozoExtra <=
-									0 ? (
-										<td className="text-center text-danger">
-											{autorizadosPorCultivo.pozoExtraAutorizada -
-												superficiePreviaCultivo.pozoExtra}
-										</td>
-									) : (
-										<td className="text-center">
-											{autorizadosPorCultivo.pozoExtraAutorizada -
-												superficiePreviaCultivo.pozoExtra}
-										</td>
-									)}
-								</tr>
+								{sistema !== "Pozo Federal" && sistema !== "Pozo Particular" && (
+									<>
+										<tr>
+											<th scope="row">Gravedad Normal</th>
+											<td className="text-center">
+												{autorizadosPorCultivo.gravedadNormalAutorizada}
+											</td>
+											<td className="text-center">
+												{autorizadosPorCultivo.gravedadNormalAsignada}
+											</td>
+											<td className="text-center">{superficiePreviaCultivo.gravedadNormal}</td>
+											{autorizadosPorCultivo.gravedadNormalAsignada -
+												superficiePreviaCultivo.gravedadNormal <=
+											0 ? (
+												<td className="text-center text-danger">
+													{autorizadosPorCultivo.gravedadNormalAsignada -
+														superficiePreviaCultivo.gravedadNormal}
+												</td>
+											) : (
+												<td className="text-center">
+													{autorizadosPorCultivo.gravedadNormalAsignada -
+														superficiePreviaCultivo.gravedadNormal}
+												</td>
+											)}
+										</tr>
+
+										<tr>
+											<th scope="row">Gravedad Extra</th>
+											<td className="text-center">
+												{autorizadosPorCultivo.gravedadExtraAutorizada}
+											</td>
+											<td className="text-center">{autorizadosPorCultivo.gravedadExtraAsignada}</td>
+											<td className="text-center">{superficiePreviaCultivo.gravedadExtra}</td>
+											{autorizadosPorCultivo.gravedadExtraAsignada -
+												superficiePreviaCultivo.gravedadExtra <=
+											0 ? (
+												<td className="text-center text-danger">
+													{autorizadosPorCultivo.gravedadExtraAsignada -
+														superficiePreviaCultivo.gravedadExtra}
+												</td>
+											) : (
+												<td className="text-center">
+													{autorizadosPorCultivo.gravedadExtraAsignada -
+														superficiePreviaCultivo.gravedadExtra}
+												</td>
+											)}
+										</tr>
+									</>
+								)}
+
+								{sistema !== "Gravedad" && (
+									<>
+										<tr>
+											<th scope="row">Pozo Normal</th>
+											<td className="text-center">{autorizadosPorCultivo.pozoNormalAutorizada}</td>
+											<td className="text-center">{autorizadosPorCultivo.pozoNormalAsignada}</td>
+											<td className="text-center">{superficiePreviaCultivo.pozoNormal}</td>
+											{autorizadosPorCultivo.pozoNormalAsignada -
+												superficiePreviaCultivo.pozoNormal <=
+											0 ? (
+												<td className="text-center text-danger">
+													{autorizadosPorCultivo.pozoNormalAsignada -
+														superficiePreviaCultivo.pozoNormal}
+												</td>
+											) : (
+												<td className="text-center">
+													{autorizadosPorCultivo.pozoNormalAsignada -
+														superficiePreviaCultivo.pozoNormal}
+												</td>
+											)}
+										</tr>
+										<tr>
+											<th scope="row">Pozo Extra</th>
+											<td className="text-center">{autorizadosPorCultivo.pozoExtraAutorizada}</td>
+											<td className="text-center">{autorizadosPorCultivo.pozoExtraAsignada}</td>
+											<td className="text-center">{superficiePreviaCultivo.pozoExtra}</td>
+											{autorizadosPorCultivo.pozoExtraAsignada -
+												superficiePreviaCultivo.pozoExtra <=
+											0 ? (
+												<td className="text-center text-danger">
+													{autorizadosPorCultivo.pozoExtraAsignada -
+														superficiePreviaCultivo.pozoExtra}
+												</td>
+											) : (
+												<td className="text-center">
+													{autorizadosPorCultivo.pozoExtraAsignada -
+														superficiePreviaCultivo.pozoExtra}
+												</td>
+											)}
+										</tr>
+									</>
+								)}
 							</tbody>
 						</table>
 					</div>
