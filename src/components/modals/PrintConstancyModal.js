399b1de9325f3c4a99d6b9aee162cd9713639450
julio <jmolina.dex@gmail.com>
2021-12-18 17:44:35 -0800

    Detalles en constancia de usuarios

--- src/components/modals/PrintConstancyModal.js
+++ src/components/modals/PrintConstancyModal.js
@@ -5,13 +5,13 @@ import { saveConstancy } from "../../helpers/DB/saveConstancy";
 export const PrintConstancyModal = ({ openModal, constancia, constancySaved, setModalState }) => {
 	const { folio, apPaterno, apMaterno, nombre, predio, tipoLocalidad } = constancia;
 	const { nombreLocalidad, fecha, supRiego, cuenta, subcta, modulo, sistemaRiego } = constancia;
-	const { municipio, estado } = constancia;
+	const { municipio, estado, reacomodo } = constancia;
 
 	const dateOptions = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
 	const customStyles = {
 		content: {
 			width: "1125px",
-			height: "1500px",
+			height: "1700px",
 			overflow: "auto"
 		}
 	};
@@ -41,175 +41,175 @@ export const PrintConstancyModal = ({ openModal, constancia, constancySaved, set
 			className="printModalSanidad p-5"
 			overlayClassName="modal-fondo"
 		>
-			<div className="row p-5 d-flex align-items-center">
-				<div className="col-5">
-					<div className="d-flex">
-						<img src={"./logos/semarnat2.png"} alt="Logo semarnat" style={{ maxHeight: 110 }} />
-					</div>
-				</div>
-
-				<div className="col-4">
-					<div className="d-flex ">
-						<img src={"./logos/cna.png"} alt="Logo cna" style={{ maxHeight: 70 }} />
-					</div>
-				</div>
-
-				<div className="col-3">
-					<div className="d-flex flex-column text-right">
-						<div>
-							<b>OFICIO:</b>
+			<div className="border border-dark rounded">
+				<div className="border border-dark rounded m-1 p-3">
+					<div className="row p-5 d-flex align-items-center">
+						<div className="col-8">
+							<div className="d-flex">
+								<img src={"./logos/medio-cna.png"} alt="Logo semarnat" style={{ maxWidth: 600 }} />
+							</div>
 						</div>
-						<div>B00.807.05.DR.014/{folio}</div>
-						<div>
-							<b>LUGAR:</b>
+
+						<div className="col-4">
+							<div className="d-flex flex-column text-right">
+								<div>
+									<b>OFICIO:</b>
+								</div>
+								<div>B00.807.05.DR.014.P.U./{folio}</div>
+								<div>
+									<b>LUGAR:</b>
+								</div>
+								<div>MEXICALI, B. C.</div>
+								<div>
+									<b>FECHA:</b>
+								</div>
+								<div>{fecha.toLocaleString("es-MX", dateOptions).toUpperCase()}</div>
+							</div>
 						</div>
-						<div>MEXICALI, B. C.</div>
-						<div>
-							<b>FECHA:</b>
-						</div>
-						<div>{fecha.toLocaleString("es-MX", dateOptions).toUpperCase()}</div>
 					</div>
-				</div>
-			</div>
 
-			<div className="row">
-				<div className="col-12">
-					<p>
-						<b>
-							ORGANISMO DE CUENCA PENÍNSULA DE BAJA CALIFORNIA
-							<br />
-							DIRECCIÓN DE INFRAESTRUCTURA HIDROAGRÍCOLA
-							<br />
-							DISTRITO DE RIEGO 014, RÍO COLORADO, B.C. Y SONORA
-							<br />
-							JEFATURA DEL PADRÓN DE USUARIOS
-						</b>
-					</p>
-				</div>
-			</div>
-
-			<div className="row pt-5"></div>
-
-			<div className="row">
-				<div className="col-12">
-					<p>
-						<b>A QUIEN CORRESPONDA:</b>
-					</p>
-				</div>
-			</div>
-
-			<div className="row pt-5"></div>
-
-			<div className="row m-3 d-flex flex-column justify-content-center mt-2">
-				<p clssName="paragraph">
-					&nbsp;&nbsp;&nbsp;&nbsp; CON FUNDAMENTO EN LOS ARTÍCULOS 52, 67 INCISO A), 70 DE LA LEY DE
-					AGUAS NACIONALES; 92 Y 102 DEL REGLAMENTO DEL DISTRITO DE RIEGO DEL RÍO COLORADO Y DEMÁS
-					RELATIVOS DE LA NORMATIVIDAD VIGENTE, ARTÍCULOS 11 LETRA B FRACCIÓN V, 73 FRACCIÓN XLI,
-					ASÍ COMO SU ÚLTIMO PÁRRAFO, 79, FRACCIÓN XVII REGLAMENTO INTERIOR DE LA COMISIÓN NACIONAL
-					DEL AGUA, CON BASE EN LA BÚSQUEDA DE LOS DATOS DEL PROMOVENTE REALIZADA EL DÍA DE HOY POR
-					EL ÁREA RESPONSABLE DEL PADRÓN DE USUARIOS (SIPAD), SE INFORMA QUE OBRA REGISTRO PADRÓN DE
-					USUARIOS DEL DISTRITO DE RIEGO 014, RÍO COLORADO, BAJA CALIFORNIA Y SONORA A NOMBRE DE{" "}
-					<b>
-						{apPaterno} {apMaterno} {nombre}
-					</b>
-					, CON EL NÚMERO DE CUENTA <b>{`${cuenta}.${subcta}`}</b>,{" "}
-					<b>EN EL MÓDULO DE RIEGO NÚMERO {modulo}</b> CON UNA SUPERFICIE DE{" "}
-					<b>{supRiego} HECTAREAS</b> DE DERECHOS DE RIEGO DE <b>{sistemaRiego.toUpperCase()}</b>,
-					EN LA <b>PARCELA NÚMERO {predio}</b>, EN{" "}
-					<b>
-						{tipoLocalidad.toUpperCase()} {nombreLocalidad}
-					</b>
-					, DEL MUNICIPIO DE <b>{municipio.toUpperCase()}</b>, <b>{estado.toUpperCase()}</b>.
-				</p>
-
-				<p clssName="paragraph">
-					&nbsp;&nbsp;&nbsp;&nbsp; LA PRESENTE SE EXTIENDE CON CARÁCTER INFORMATIVO A PETICIÓN DEL
-					INTERESADO PARA LOS FINES QUE ESTIME CONVENIENTES Y SE LE RECUERDA QUE DE ACUERDO A LA
-					NORMATIVA EN MATERIA DE AGUAS ES DEBER DEL USUARIO, CONCESIONARIO O ASIGNATARIO, SEGUIR
-					LOS TRÁMITES Y PROCEDIMIENTOS ADMINISTRATIVOS RELATIVOS PARA LA ACTUALIZACIÓN DE LA
-					INFORMACIÓN, TRANSMISIÓN, CESIÓN DE DERECHOS DE AGUA EN EL PADRÓN.
-				</p>
-
-				<p clssName="paragraph">
-					&nbsp;&nbsp;&nbsp;&nbsp; LA CONSTANCIA SE EMITE, SIN PREJUZGAR SOBRE LA EXISTENCIA DE
-					ALGUNA OTRA SOLICITUD, PROCEDIMIENTO ADMINISTRATIVO, SUCESORIO O JUDICIAL QUE NO SE HAYA
-					HECHO DE CONOCIMIENTO AL MOMENTO DE FORMULAR SU PETICIÓN.
-				</p>
-			</div>
-
-			<div className="row pt-5"></div>
-
-			<div className="row pt-5">
-				<p>
-					<b>
-						<div className="col-12">ATENTAMENTE</div>
-					</b>
-				</p>
-			</div>
-
-			<div className="row d-flex mt-5">
-				<p>
-					<b>
+					<div className="row">
 						<div className="col-12">
-							DR. JULIO ALFONSO NAVARRO URBINA,
-							<br />
-							ING. EN JEFE DEL DISTRITO DE RIEGO 014,
-							<br />
-							RÍO COLORADO, BAJA CALIFORNIA Y SONORA
+							<p>
+								<b>
+									ORGANISMO DE CUENCA PENÍNSULA DE BAJA CALIFORNIA
+									<br />
+									DIRECCIÓN DE INFRAESTRUCTURA HIDROAGRÍCOLA
+									<br />
+									DISTRITO DE RIEGO 014, RÍO COLORADO, B.C. Y SONORA
+									<br />
+									JEFATURA DEL PADRÓN DE USUARIOS
+								</b>
+							</p>
 						</div>
-					</b>
-				</p>
-			</div>
-
-			<div className="row m-3 mt-5">
-				<b>
-					<div className="col-12">
-						C.c.p.- Archivo <br />
-						VMMR/JDLA/pcf
 					</div>
-				</b>
-			</div>
-			<div className="row">
-				<div className="col-10 d-flex">
-					<div className=" align-self-end">
-						Avenida Insurgentes Sur Número 2416, Colonia Copilco el Bajo, Alcaldía Coyoacán, Código
-						postal 04340, Ciudad de México. Teléfono: 55 5174 4000 www.gob.mx/conagua
-						<hr />
+
+					<div className="row pt-5"></div>
+
+					<div className="row">
+						<div className="col-12">
+							<p>
+								<b>A QUIEN CORRESPONDA:</b>
+							</p>
+						</div>
+					</div>
+
+					<div className="row m-3 d-flex flex-column justify-content-center mt-2">
+						<p clssName="paragraph">
+							&nbsp;&nbsp;&nbsp;&nbsp; CON FUNDAMENTO EN LOS ARTÍCULOS 52, 67 INCISO A), 70 DE LA
+							LEY DE AGUAS NACIONALES; 92 Y 102 DEL REGLAMENTO DEL DISTRITO DE RIEGO DEL RÍO
+							COLORADO Y DEMÁS RELATIVOS DE LA NORMATIVIDAD VIGENTE, ARTÍCULOS 11 LETRA B FRACCIÓN
+							V, 73 FRACCIÓN XLI, ASÍ COMO SU ÚLTIMO PÁRRAFO, 79, FRACCIÓN XVII REGLAMENTO INTERIOR
+							DE LA COMISIÓN NACIONAL DEL AGUA, CON BASE EN LA BÚSQUEDA DE LOS DATOS DEL PROMOVENTE
+							REALIZADA EL DÍA DE HOY POR EL ÁREA RESPONSABLE DEL PADRÓN DE USUARIOS (SIPAD), SE
+							INFORMA QUE OBRA REGISTRO PADRÓN DE USUARIOS DEL DISTRITO DE RIEGO 014, RÍO COLORADO,
+							BAJA CALIFORNIA Y SONORA A NOMBRE DE{" "}
+							<b>
+								{apPaterno} {apMaterno} {nombre}
+							</b>
+							, CON EL NÚMERO DE CUENTA <b>{`${cuenta}.${subcta}`}</b>,{" "}
+							<b>EN EL MÓDULO DE RIEGO NÚMERO {modulo}</b> CON UNA SUPERFICIE DE{" "}
+							<b>{supRiego} HECTAREAS</b> DE DERECHOS DE RIEGO DE{" "}
+							<b>{sistemaRiego.toUpperCase()}</b>, EN LA <b>PARCELA NÚMERO {predio}</b>, EN{" "}
+							<b>
+								{tipoLocalidad.toUpperCase()} {nombreLocalidad}
+							</b>
+							{reacomodo.length > 0 && <b> (REACOMODO {reacomodo})</b>}, DEL MUNICIPIO DE{" "}
+							<b>{municipio.toUpperCase()}</b>, <b>{estado.toUpperCase()}</b>.
+						</p>
+
+						<p clssName="paragraph">
+							&nbsp;&nbsp;&nbsp;&nbsp; LA PRESENTE SE EXTIENDE CON CARÁCTER INFORMATIVO A PETICIÓN
+							DEL INTERESADO PARA LOS FINES QUE ESTIME CONVENIENTES Y SE LE RECUERDA QUE DE ACUERDO
+							A LA NORMATIVA EN MATERIA DE AGUAS ES DEBER DEL USUARIO, CONCESIONARIO O ASIGNATARIO,
+							SEGUIR LOS TRÁMITES Y PROCEDIMIENTOS ADMINISTRATIVOS RELATIVOS PARA LA ACTUALIZACIÓN
+							DE LA INFORMACIÓN, TRANSMISIÓN, CESIÓN DE DERECHOS DE AGUA EN EL PADRÓN.
+						</p>
+
+						<p clssName="paragraph">
+							&nbsp;&nbsp;&nbsp;&nbsp; LA CONSTANCIA SE EMITE, SIN PREJUZGAR SOBRE LA EXISTENCIA DE
+							ALGUNA OTRA SOLICITUD, PROCEDIMIENTO ADMINISTRATIVO, SUCESORIO O JUDICIAL QUE NO SE
+							HAYA HECHO DE CONOCIMIENTO AL MOMENTO DE FORMULAR SU PETICIÓN.
+						</p>
+					</div>
+
+					<div className="row pt-4">
+						<p>
+							<b>
+								<div className="col-12">ATENTAMENTE</div>
+							</b>
+						</p>
+					</div>
+
+					<div className="row d-flex mt-4">
+						<p>
+							<b>
+								<div className="col-12">
+									DR. JULIO ALFONSO NAVARRO URBINA,
+									<br />
+									ING. EN JEFE DEL DISTRITO DE RIEGO 014,
+									<br />
+									RÍO COLORADO, BAJA CALIFORNIA Y SONORA
+								</div>
+							</b>
+						</p>
+					</div>
+
+					<div className="row m-3 mt-4">
+						<b>
+							<div className="col-12">
+								C.c.p.- Archivo <br />
+								VMMR/JDLA/pcf
+							</div>
+						</b>
+					</div>
+					<div className="row">
+						<div className="col-10 d-flex">
+							<div className="align-self-center pl-4 pr-4 pb-2">
+								<div>Av. Reforma y calle “L” s/n Col. Nueva, Mexicali, Baja California.</div>
+								<div>Teléfono: (686) 551 64 00. </div>
+								<div>http//:www.gob.mx/conagua</div>
+							</div>
+						</div>
+
+						<div className="col-2">
+							<div className="d-flex justify-content-end">
+								<img src={"./logos/pie.png"} alt="Logo mexico" style={{ maxWidth: 970 }} />
+							</div>
+						</div>
+					</div>
+
+					<div className="row m-3 d-flex justify-content-center pt-4">
+						{constancySaved ? (
+							<button
+								type="button"
+								className="btn btn-outline-primary d-print-none"
+								onClick={imprimir}
+							>
+								<i className="fas fa-print"></i>
+								<span> Imprimir</span>
+							</button>
+						) : (
+							<button
+								type="button"
+								className="btn btn-outline-primary ml-5 d-print-none"
+								onClick={handleSaveConstancy}
+							>
+								<i className="fas fa-save"></i>
+								<span> Guardar </span>
+							</button>
+						)}
+
+						<button
+							type="button"
+							className="btn btn-outline-primary ml-5 d-print-none ml-3"
+							onClick={closeModal}
+						>
+							<i className="fas fa-sign-out-alt"></i>
+							<span> Cerrar</span>
+						</button>
 					</div>
 				</div>
-
-				<div className="col-2">
-					<div className="d-flex justify-content-end">
-						<img src={"./logos/mexico2021.jpg"} alt="Logo mexico" style={{ maxHeight: 170 }} />
-					</div>
-				</div>
-			</div>
-
-			<div className="row m-3 d-flex justify-content-center pt-5">
-				{constancySaved ? (
-					<button type="button" className="btn btn-outline-primary d-print-none" onClick={imprimir}>
-						<i className="fas fa-print"></i>
-						<span> Imprimir</span>
-					</button>
-				) : (
-					<button
-						type="button"
-						className="btn btn-outline-primary ml-5 d-print-none"
-						onClick={handleSaveConstancy}
-					>
-						<i className="fas fa-save"></i>
-						<span> Guardar </span>
-					</button>
-				)}
-
-				<button
-					type="button"
-					className="btn btn-outline-primary ml-5 d-print-none ml-3"
-					onClick={closeModal}
-				>
-					<i className="fas fa-sign-out-alt"></i>
-					<span> Cerrar</span>
-				</button>
 			</div>
 		</Modal>
 	);
