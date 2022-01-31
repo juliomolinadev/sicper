95511e13740334c1c948684415379bbe24d2bc5b
julio <jmolina.dex@gmail.com>
2022-01-30 23:21:39 -0800

    Updates

---
+++ precache-manifest.e5b2b7125225c32df8582d021dec4671.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97a78554992a7a9c74e45e8a104c8490",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f49b2762b3ad0cc9195e",
+    "url": "/static/css/2.edba33d2.chunk.css"
+  },
+  {
+    "revision": "2c54a3d227dc31e32844",
+    "url": "/static/css/main.bbe39ac4.chunk.css"
+  },
+  {
+    "revision": "f49b2762b3ad0cc9195e",
+    "url": "/static/js/2.12941b57.chunk.js"
+  },
+  {
+    "revision": "10a280b98dbf2de163a8b88e212b76e2",
+    "url": "/static/js/2.12941b57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c54a3d227dc31e32844",
+    "url": "/static/js/main.53aea223.chunk.js"
+  },
+  {
+    "revision": "625fe777cd15a7f7e360",
+    "url": "/static/js/runtime-main.fcf44fbc.js"
+  }
+]);
