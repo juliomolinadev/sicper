23833dfb61a0eae90a0ffe24b7df887be4e8886f
julio <jmolina.dex@gmail.com>
2022-05-30 19:01:42 -0700

    Restringe generacion de guia solo para trigo

--- src/components/permisos/PermisosScreen.js
+++ src/components/permisos/PermisosScreen.js
@@ -40,6 +40,8 @@ export const PermisosScreen = () => {
 	const { variablesGlobales } = useSelector((state) => state.auth);
 	const { cicloActual: ciclo, expedicionActiva } = variablesGlobales;
 
+	const cultivosConGuia = [3];
+
 	useEffect(() => {
 		dispatch(startLoadPermisos(modulo, ciclo));
 		dispatch(startLoadSuperficies(modulo, ciclo));
@@ -278,27 +280,31 @@ export const PermisosScreen = () => {
 									</button>
 								</div>
 
-								<div className="col-4 d-flex justify-content-center">
-									{dataPermiso.estadoPermiso === "activo" && privilegios.solicitarGuias && guia ? (
-										<button
-											type="button"
-											className="btn btn-sm btn-outline-info"
-											onClick={handleOpenPrintGuiaModal}
-										>
-											<i className="fas fa-file"></i>
-											<span> Imprimir Guía</span>
-										</button>
-									) : (
-										<button
-											type="button"
-											className="btn btn-sm btn-outline-info"
-											onClick={startOpenGuiaForm}
-										>
-											<i className="fas fa-file"></i>
-											<span> Generar Guía</span>
-										</button>
-									)}
-								</div>
+								{cultivosConGuia.includes(dataPermiso.claveCultivo) && (
+									<div className="col-4 d-flex justify-content-center">
+										{dataPermiso.estadoPermiso === "activo" &&
+										privilegios.solicitarGuias &&
+										guia ? (
+											<button
+												type="button"
+												className="btn btn-sm btn-outline-info"
+												onClick={handleOpenPrintGuiaModal}
+											>
+												<i className="fas fa-file"></i>
+												<span> Imprimir Guía</span>
+											</button>
+										) : (
+											<button
+												type="button"
+												className="btn btn-sm btn-outline-info"
+												onClick={startOpenGuiaForm}
+											>
+												<i className="fas fa-file"></i>
+												<span> Generar Guía</span>
+											</button>
+										)}
+									</div>
+								)}
 
 								<div className="col-4 d-flex justify-content-center">
 									{dataPermiso.estadoPermiso === "activo" && privilegios.solicitarCancelarPermisos && (
