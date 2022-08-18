7ccdc9429251c3a91061f9a290a9bc1d9b236f78
julio <jmolina.dex@gmail.com>
2022-08-18 15:22:05 -0700

    Servicio suspendido

--- src/SicperApp.js
+++ src/SicperApp.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Provider } from "react-redux";
 
 import { store } from "./store/store";
-import { AppRouter } from "./routers/AppRouter";
+// import { AppRouter } from "./routers/AppRouter";
 // import { Page404 } from "./scenes/Page404";
+import { ServicioSuspendido } from "./scenes/ServicioSuspendido";
 
 export const SicperApp = () => {
 	return (
@@ -11,7 +12,8 @@ export const SicperApp = () => {
 			<script srs="filesaver.js"></script>
 			<Provider store={store}>
 				{/* <Page404 /> */}
-				<AppRouter />
+				{/* <AppRouter /> */}
+				<ServicioSuspendido />
 			</Provider>
 		</div>
 	);
---
+++ src/scenes/ServicioSuspendido.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ServicioSuspendido = () => {
+	return (
+		<div className="d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
+			<h1 className="border border-warning rounded text-warning p-5">Servicio Suspendido !</h1>
+		</div>
+	);
+};
